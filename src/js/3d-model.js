fe79aef820d71291894499282d124b3cf82bc332
ArtZ <snickgem@gmail.com>
2024-09-17 22:16:11 +0300

    create cube

--- src/js/3d-model.js
+++ src/js/3d-model.js
@@ -1,15 +1,24 @@
 import * as THREE from './three.module.min.js';
 import {OrbitControls} from './OrbitControls.js';
+import {Line2} from './Line2.js';
+import {LineGeometry} from './LineGeometry.js';
+import {LineMaterial} from './LineMaterial.js';
 import TWEEN from './tween.module.js';
 
 const w1600 = window.matchMedia("(min-width: 1600px)");
 const w1300 = window.matchMedia("(min-width: 1300px)");
 const w768 = window.matchMedia("(min-width: 768px)");
 const w576 = window.matchMedia("(min-width: 576px)");
-const ANGLES_NUM = 5; // Количество углов у плоской фигуры
-const FIGURE_HEIGHT = w768.matches ? 5.5 : w576.matches ? 13 : 16; // Высота объемной фигуры
-const FIGURE_RADIUS = w768.matches ? 5 : w576.matches ? 4 : 3; // Радиус
-const CURV_COEF = w768.matches ? 2 : 1.3; // Коэффициент отклонения углов
+const ANGLES_NUM = 4; // Количество углов у плоской фигуры
+const FIGURE_HEIGHT = w768.matches ? 5 : w576.matches ? 13 : 16; // Высота объемной фигуры
+const SQUARE = [
+	{x: 5, y: 5},
+	{x: 5, y: -5},
+	{x: -5, y: -5},
+	{x: -5, y: 5},
+	{x: 5, y: 5}
+] //Координаты квадрата
+const RAD = (Math.PI / 180) * 35; // Угол поворота в радианах
 const LINES_COLOR = "#ffffff"; // Цвет линий
 const SPHERE_COLORS = [
 	"#29b579",
@@ -29,13 +38,21 @@ const tabs = document.querySelectorAll(".graphic__tab");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
 if (w1600.matches) {
-	camera.position.set(-0.19058996153741722, -8.456740289133542, 1.5396789741867494);
+	camera.position.set(-0.01993214386260716, -11.40833628815598, 1.0599760086136567);
 } else if (w1300.matches) {
 	camera.position.set(-0.19058996153741642, -11.157212056000972, 2.783018838956491);
 } else {
 	camera.position.set(-0.1905899615374156, -18.100924888631944, 5.980013927091829);
 }
 camera.aspect = parent.offsetWidth / parent.offsetWidth;
+
+/*camera.lookAt(new THREE.Vector3(-0.1054263625772272, -26.472392884690667, 8.203219385364957));
+new TWEEN.Tween(camera.position).to({
+	x: -0.01993214386260716,
+	y: -11.40833628815598,
+	z: 1.0599760086136567
+}, 1000).start();*/
+
 const renderer = new THREE.WebGLRenderer({
 	antialias: true
 });
@@ -49,7 +66,7 @@ renderer.setClearColor(0x403f4d, 0);
 parent.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(-0.19058996153741756, -4.083425180755162, -0.473864393368694);
+controls.target.set(0.01800548093765126, -4.723737777035119, -2.1098019150692564);
 controls.update();
 
 controls.addEventListener("change", () => {
@@ -57,76 +74,128 @@ controls.addEventListener("change", () => {
 	console.log(controls.target)
 	console.log("camera.position")
 	console.log(camera.position)
+
+	//controls.target.x = 0;
+	//camera.position.x = 0;
 })
 
-/* Цвет линий */
-const materialLine = new THREE.LineBasicMaterial({
-	color: LINES_COLOR
+/* Материал каркасных линий */
+const materialLine = new LineMaterial({
+	color: LINES_COLOR,
+	linewidth: 3
 });
 
-/* Верхний пятиугольник */
+/* Материал внутренних линий */
+const materialLine2 = new LineMaterial({
+	color: "#ffffff",
+	linewidth: 1
+});
+
+/*const grid = new THREE.GridHelper(50, 50, "#000000", "#000000");
+grid.rotateX(90);
+grid.position.y = 40;
+scene.add(grid);*/
+
+/* Верхний квадрат */
 const polygonCoords1 = [];
 for (let i = 0; i <= ANGLES_NUM; i++) {
-	const alpha = 2 * i * Math.PI / ANGLES_NUM;
 	polygonCoords1.push(
-		new THREE.Vector3(
-			i === 1 || i === 4 ? Math.sin(alpha) * FIGURE_RADIUS * CURV_COEF : Math.sin(alpha) * FIGURE_RADIUS,
-			-Math.cos(alpha) * FIGURE_RADIUS,
+			SQUARE[i].x * Math.cos(RAD) - SQUARE[i].y * Math.sin(RAD),
+			SQUARE[i].x * Math.sin(RAD) + SQUARE[i].y * Math.cos(RAD),
 			0
-		)
 	);
 }
-const geometry1 = new THREE.BufferGeometry().setFromPoints(polygonCoords1);
-const polygon1 = new THREE.Line(geometry1, materialLine);
-polygon1.rotateX(-Math.PI);
+const geometry1 = new LineGeometry();
+geometry1.setPositions(polygonCoords1);
+const polygon1 = new Line2(geometry1, materialLine);
 scene.add(polygon1);
 
-/* Нижний пятиугольник */
+/* Нижний квадрат */
 const polygonCoords2 = [];
 for (let i = 0; i <= ANGLES_NUM; i++) {
-	const alpha = 2 * i * Math.PI / ANGLES_NUM;
 	polygonCoords2.push(
-		new THREE.Vector3(
-			i === 1 || i === 4 ? Math.sin(alpha) * FIGURE_RADIUS * CURV_COEF : Math.sin(alpha) * FIGURE_RADIUS,
-			-Math.cos(alpha) * FIGURE_RADIUS,
-			FIGURE_HEIGHT
-		)
+			SQUARE[i].x * Math.cos(RAD) - SQUARE[i].y * Math.sin(RAD),
+			SQUARE[i].x * Math.sin(RAD) + SQUARE[i].y * Math.cos(RAD),
+			-FIGURE_HEIGHT
 	);
 }
-const geometry2 = new THREE.BufferGeometry().setFromPoints(polygonCoords2);
-const polygon2 = new THREE.Line(geometry2, materialLine);
-polygon2.rotateX(-Math.PI);
+const geometry2 = new LineGeometry();
+geometry2.setPositions(polygonCoords2);
+const polygon2 = new Line2(geometry2, materialLine);
 scene.add(polygon2);
 
-/* Ребра между пятиугольниками */
+/* Ребра между квадратами */
 for (let linesCount = 0; linesCount < ANGLES_NUM; linesCount++) {
 	const linesCoords = [];
 
-	for (let i = 0; i < 10; i++) {
-		const alpha = 2 * linesCount * Math.PI / ANGLES_NUM;
-		linesCoords.push([
-			new THREE.Vector3(
-				linesCount === 1 || linesCount === 4 ? Math.sin(alpha) * FIGURE_RADIUS * CURV_COEF : Math.sin(alpha) * FIGURE_RADIUS,
-				-Math.cos(alpha) * FIGURE_RADIUS,
-				0
-			),
-			new THREE.Vector3(
-				linesCount === 1 || linesCount === 4 ? Math.sin(alpha) * FIGURE_RADIUS * CURV_COEF : Math.sin(alpha) * FIGURE_RADIUS,
-				-Math.cos(alpha) * FIGURE_RADIUS,
-				FIGURE_HEIGHT
-			)
-		]);
-	}
+	linesCoords.push(
+		SQUARE[linesCount].x * Math.cos(RAD) - SQUARE[linesCount].y * Math.sin(RAD),
+		SQUARE[linesCount].x * Math.sin(RAD) + SQUARE[linesCount].y * Math.cos(RAD),
+		0,
+		SQUARE[linesCount].x * Math.cos(RAD) - SQUARE[linesCount].y * Math.sin(RAD),
+		SQUARE[linesCount].x * Math.sin(RAD) + SQUARE[linesCount].y * Math.cos(RAD),
+		-FIGURE_HEIGHT
+	);
 
-	const geometryEdges = new THREE.BufferGeometry().setFromPoints(linesCoords[linesCount]);
-	const edges = new THREE.Line(geometryEdges, materialLine);
-	edges.rotateX(-Math.PI);
+	const geometryEdges = new LineGeometry();
+	geometryEdges.setPositions(linesCoords);
+	const edges = new Line2(geometryEdges, materialLine);
 	scene.add(edges);
 }
 
+/* Сплошные проходящие через центр плоскостей */
+for (let linesCount = 0; linesCount < ANGLES_NUM * 2; linesCount++) {
+	const centerCoords = [];
+	if (linesCount < ANGLES_NUM) { // По бокам
+		if (linesCount % 2 === 0) {
+			centerCoords.push(
+				SQUARE[linesCount].x * Math.cos(RAD),
+				SQUARE[linesCount].y * Math.sin(RAD),
+				0,
+				SQUARE[linesCount].x * Math.cos(RAD),
+				SQUARE[linesCount].y * Math.sin(RAD),
+				-FIGURE_HEIGHT
+			);
+		} else {
+			centerCoords.push(
+				SQUARE[linesCount].x * Math.sin(RAD),
+				SQUARE[linesCount].y * Math.cos(RAD),
+				0,
+				SQUARE[linesCount].x * Math.sin(RAD),
+				SQUARE[linesCount].y * Math.cos(RAD),
+				-FIGURE_HEIGHT
+			);
+		}
+	} else { // Сверху
+		if (linesCount % 2 === 0) {
+			centerCoords.push(
+				SQUARE[linesCount-4].x * Math.cos(RAD),
+				SQUARE[linesCount-4].y * Math.sin(RAD),
+				linesCount < (ANGLES_NUM + 2) ? -FIGURE_HEIGHT : 0,
+				SQUARE[linesCount-4].x * Math.cos(RAD) - SQUARE[linesCount-4].y * Math.cos(RAD) * 2,
+				SQUARE[linesCount-4].y * Math.sin(RAD) - SQUARE[linesCount-4].y * Math.sin(RAD) * 2,
+				linesCount < (ANGLES_NUM + 2) ? -FIGURE_HEIGHT : 0
+			);
+		} else {
+			centerCoords.push(
+				SQUARE[linesCount-4].x * Math.sin(RAD),
+				SQUARE[linesCount-4].y * Math.cos(RAD),
+				linesCount < (ANGLES_NUM + 3) ? -FIGURE_HEIGHT : 0,
+				SQUARE[linesCount-4].x * Math.sin(RAD) + SQUARE[linesCount-4].y * Math.sin(RAD) * 2,
+				SQUARE[linesCount-4].y * Math.cos(RAD) - SQUARE[linesCount-4].y * Math.cos(RAD) * 2,
+				linesCount < (ANGLES_NUM + 3) ? -FIGURE_HEIGHT : 0
+			);
+		}
+	}
+
+	const geometryCenter = new LineGeometry();
+	geometryCenter.setPositions(centerCoords);
+	const center = new Line2(geometryCenter, materialLine2);
+	scene.add(center);
+}
+
 /* Сферы атомы */
-for (let i = 0; i < ANGLES_NUM * 2; i++) {
-	const alpha = 2 * i * Math.PI / ANGLES_NUM;
+for (let i = 0; i < ANGLES_NUM * 2 + 2; i++) {
 	const geometry = new THREE.SphereGeometry(0.3, 32, 16);
 	const materialBox = new THREE.MeshBasicMaterial({
 		color: SPHERE_COLORS[i],
@@ -135,20 +204,22 @@ for (let i = 0; i < ANGLES_NUM * 2; i++) {
 	const sphere = new THREE.Mesh(geometry, materialBox);
 	sphere.userData = {"data-tab": `tab-${i+1}`, "r": `${sphere.material.color.r}`, "g": `${sphere.material.color.g}`, "b": `${sphere.material.color.b}`}; // Ставим дата аттрибуты для табов и исходного цвета
 
-	// Первую половину атомов располагаем на верхней плоскости, вторую - на нижней
-	if (i < ANGLES_NUM) {
-		// -Math.PI * 0.31 - координаты атома с учетом поворота остальных частей фигуры
+	if (i < ANGLES_NUM) { // Атомы для верхней плоскости
 		sphere.position.set(
-			(i === 1 || i === 4) ? Math.sin(alpha) * FIGURE_RADIUS * -Math.PI * 0.315 * CURV_COEF : Math.sin(alpha) * FIGURE_RADIUS * -Math.PI * 0.31,
-			-Math.cos(alpha) * FIGURE_RADIUS * -Math.PI * 0.31,
+			SQUARE[i].x * Math.cos(RAD) - SQUARE[i].y * Math.sin(RAD),
+			SQUARE[i].x * Math.sin(RAD) + SQUARE[i].y * Math.cos(RAD),
 			0
 		);
-	} else {
+	} else if (i < ANGLES_NUM * 2) { // Атомы для нижней плоскости
 		sphere.position.set(
-			(i === 6 || i === 9) ? Math.sin(alpha) * FIGURE_RADIUS * -Math.PI * 0.315 * CURV_COEF : Math.sin(alpha) * FIGURE_RADIUS * -Math.PI * 0.31,
-			-Math.cos(alpha) * FIGURE_RADIUS * -Math.PI * 0.31,
+			SQUARE[i-3].x * Math.cos(RAD) - SQUARE[i-3].y * Math.sin(RAD),
+			SQUARE[i-3].x * Math.sin(RAD) + SQUARE[i-3].y * Math.cos(RAD),
 			-FIGURE_HEIGHT
 		);
+	} else if (i === ANGLES_NUM * 2 + 1) { // Атомы для центра верхней плоскости
+		sphere.position.set(0, 0, 0);
+	} else { // Атомы для центра нижней плоскости
+		sphere.position.set(0, 0, -FIGURE_HEIGHT);
 	}
 	scene.add(sphere);
 }
@@ -229,7 +300,7 @@ const animationClickHandler = (e) => {
 		-(((e.clientY - renderer.domElement.getBoundingClientRect().top) / renderer.domElement.clientHeight) * 2) + 1,
 	);
 	const triggeredElems = scene.children.filter((el) => {
-		return el.isMesh
+		return Object.keys(el.userData).length !== 0;
 	});
 
 	raycaster.setFromCamera(coords, camera);
